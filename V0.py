d6ba527fba4223bb5d40b8585a7621033580d0ae
0-122-1 <0-122-1>
2020-09-24 18:17:16 +0500

    Organise all.

--- V0.py
+++
@@ -1,8 +0,0 @@
-import random, string, re
-
-def saveStringReversedFile(string):
-    with open(input("Enter file name: "), 'w') as file:
-        file.write(string[::-1]) 
-    
-saveStringReversedFile("".join(re.findall('[0-9]', "".join(random.choice(string.ascii_uppercase + \
-                                                                         string.ascii_lowercase + string.digits) for x in range(20)))))
